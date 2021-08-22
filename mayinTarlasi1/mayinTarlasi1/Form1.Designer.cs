a3cd5848fe48d4f2b2becd5e31bfc353528e86fd
Oğuzhan <86168329+oguzhanaykuttt@users.noreply.github.com>
2021-08-22 06:46:04 +0300

    minefield-form
    3 farklı aşamalı winform mayın tarlası uygulaması

---
+++ mayinTarlasi1/mayinTarlasi1/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace mayinTarlasi1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.yeniOyunToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kolayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ortaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yeniOyunToolStripMenuItem});
+            this.menuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.Flow;
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(632, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // yeniOyunToolStripMenuItem
+            // 
+            this.yeniOyunToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kolayToolStripMenuItem,
+            this.ortaToolStripMenuItem,
+            this.zorToolStripMenuItem});
+            this.yeniOyunToolStripMenuItem.Name = "yeniOyunToolStripMenuItem";
+            this.yeniOyunToolStripMenuItem.Size = new System.Drawing.Size(88, 24);
+            this.yeniOyunToolStripMenuItem.Text = "Yeni Oyun";
+            // 
+            // kolayToolStripMenuItem
+            // 
+            this.kolayToolStripMenuItem.Name = "kolayToolStripMenuItem";
+            this.kolayToolStripMenuItem.Size = new System.Drawing.Size(129, 26);
+            this.kolayToolStripMenuItem.Text = "Kolay";
+            this.kolayToolStripMenuItem.Click += new System.EventHandler(this.kolayToolStripMenuItem_Click);
+            // 
+            // ortaToolStripMenuItem
+            // 
+            this.ortaToolStripMenuItem.Name = "ortaToolStripMenuItem";
+            this.ortaToolStripMenuItem.Size = new System.Drawing.Size(129, 26);
+            this.ortaToolStripMenuItem.Text = "Orta";
+            this.ortaToolStripMenuItem.Click += new System.EventHandler(this.ortaToolStripMenuItem_Click);
+            // 
+            // zorToolStripMenuItem
+            // 
+            this.zorToolStripMenuItem.Name = "zorToolStripMenuItem";
+            this.zorToolStripMenuItem.Size = new System.Drawing.Size(129, 26);
+            this.zorToolStripMenuItem.Text = "Zor";
+            this.zorToolStripMenuItem.Click += new System.EventHandler(this.zorToolStripMenuItem_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 31);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(600, 600);
+            this.flowLayoutPanel1.TabIndex = 1;
+            this.flowLayoutPanel1.TabStop = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(632, 603);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem yeniOyunToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kolayToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ortaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zorToolStripMenuItem;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ mayinTarlasi1/mayinTarlasi1/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mayinTarlasi1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void kolayToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Tarla(10, 10);
+        }        
+        private void ortaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Tarla(13, 13);
+        }
+
+        private void zorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Tarla(16, 16);
+        }
+        void Tarla(int satir, int sutun)
+        {
+            flowLayoutPanel1.Controls.Clear();
+            int mayin = satir * sutun / 10;
+            int[] mayinlar = new int[mayin];
+            Random rnd = new Random();
+            for (int i = 0; i < mayinlar.Length; i++)
+            {
+                int secilen = rnd.Next(0, satir * sutun);
+                if (mayinlar.Contains(secilen) == true)
+                {
+                    i--;
+                }
+                else
+                {
+                    mayinlar[i] = secilen;
+                }
+            }
+            for (int i = 0; i < satir * sutun; i++)
+            {
+                Button btn = new Button();
+                btn.Width = btn.Height = 30;
+                btn.BackColor = Color.Green;
+                if (mayinlar.Contains(i) == true)
+                    btn.Tag = true;
+                else
+                    btn.Tag = false;
+                btn.Click += btn_Click;
+                btn.Margin = new Padding(5, 5, 0, 0);
+                flowLayoutPanel1.Controls.Add(btn);
+            }
+            flowLayoutPanel1.Width = sutun * 35;
+            flowLayoutPanel1.Width = satir * 35;
+            this.Width = flowLayoutPanel1.Width + 50;
+            this.Height = flowLayoutPanel1.Height + 80;
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Button tiklanan = sender as Button;
+            if ((bool)tiklanan.Tag==true)
+            {
+                foreach (Control item in flowLayoutPanel1.Controls)
+                {
+                    Button btn = item as Button;
+                    if ((bool)btn.Tag==true)
+                    {
+                        btn.BackColor=Color.DarkRed;
+
+                    }
+                    else
+                    {
+                        btn.BackColor = Color.Green;
+                    }
+                }
+                MessageBox.Show("Oyun Bitti. Yeni Oyun Başlatın!");
+                flowLayoutPanel1.Controls.Clear();
+            }
+            else
+            {
+                tiklanan.BackColor = Color.Gray;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
